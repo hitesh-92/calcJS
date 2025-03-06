a1c97a881cf112ece8265db42e1332e00f4218d3
Hitesh Gohil <hiteshgohil@hotmail.co.uk>
2025-03-06 14:13:46 +0000

    js file linked, bug fixed

--- public/main.js
+++ public/main.js
@@ -1,52 +1,63 @@
 //Functional programming because I don't know how to use OOP javascipt
 
-alert("TESTING JS")
+console.log("MAIN.JS running")
 
 let sum = undefined
 let userInput = undefined
 let display = ""
 let displayOperator = ""
 
-const resetUserInput = () => userInput= ""
-const resetOperator = () => displayOperator = ""
-const resetSum = () => sum = undefined
-const resetDisplay = () => display = ""
+/*const button = document.getElementById("three")
+button.addEventListener('click', () => {
+  console.log("THEEEE")
+  alert("pressed Three")
+  calc()
+})
+*/
 
 function calc(user_operator, firstInput, secondInput){
 
   const operator = user_selected_operation(user_operator)
 
-  document.getElementById("display").innerHTML = "HITESH"
+  //ocument.getElementById("display").innerHTML = "HITESH"
 
   const result = 0
 
   console.log("CALC RUNNING")
 }
 
-const button = document.getElementById("three")
-button.addEventListener('click', () => {
-  console.log("THEEEE")
-  alert("pressed Three")
-})
 
-
-function user_selected_operation(operstionInput){
+function user_selected_operation(operationInput){
 
   const addition = (x, sum) => x + sum
   const minus = (x, sum) => sum - x
   const multiply = (x, sum) => x * sum
   const divide = (x, sum) => sum / x
 
-  swtich(operationInput){
+  switch(operationInput){
     case "+":
-      return multiply
+      return addition
     case "-":
       return minus
     case "*":
       return multiply
     case "/":
       return divide
-
   }
+
+}
+
+
+function resetInputs({userInput, displayOperator, sum, display}){
+
+  const resetUserInput = () => userInput = ""
+  const resetOperator = () => displayOperator = ""
+  const resetSum = () => sum = undefined
+  const resetDisplay = () => display = ""
+
+  resetUserInput()
+  resetOperator()
+  resetSum()
+  resetDisplay()
 
 }
